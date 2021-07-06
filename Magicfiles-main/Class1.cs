888b10d62fb013cb375d349f37947d1063d951c7
aryangautam9425 <68505143+aryangautam9425@users.noreply.github.com>
2021-07-06 08:24:39 +0530

    Add files via upload

---
+++ Magicfiles-main/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;    
+
+namespace MagicFilesLib
+{
+    public interface IDirectoryExplorer
+
+    {
+
+        ICollection<string> GetFile(string path);
+
+    }
+    public class DirectoryExplorer 
+
+    {
+        IDirectoryExplorer _directoryExplorer;
+       public DirectoryExplorer(IDirectoryExplorer directoryExplorer)
+        {
+            _directoryExplorer = directoryExplorer;
+        }
+
+        public ICollection<string> GetFiles(string path)
+
+        {
+
+            var files = _directoryExplorer.GetFile(path);
+
+            return files;
+
+        }
+
+    }
+}
